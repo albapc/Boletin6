d892811bf28ae096631126212a578367f1aaeb66
aperezcesar <aperezcesar@nautilus1.danielcastelao.org>
2018-10-30 13:18:34 +0100

    en proceso

---
+++ boletin6_6/src/boletin6_6/Boletin6_6.java
@@ -0,0 +1,27 @@
+
+package boletin6_6;
+
+import java.util.Scanner;
+
+public class Boletin6_6 {
+
+    public static void main(String[] args) {
+     Scanner sc= new Scanner (System.in);
+     String nomArtigo;
+        System.out.println("Introduce o nome do producto:");
+        nomArtigo= sc.nextLine();
+        
+    int vendasAnuais;
+        System.out.println("Introduce o número de vendas anuais:");
+        vendasAnuais= sc.nextInt();
+    
+    if(vendasAnuais<=100)
+        System.out.println("Artigo de consumo baixo");
+    else if vendasAnuais>100 & <=500{
+            
+        }
+    }
+        
+    }
+    
+}
