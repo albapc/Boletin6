ed76df9fc898d57d51c09b05d5cfcf978cae21b2
albapc <alba.perez.ce@gmail.com>
2018-11-02 01:12:42 +0100

    terminar

--- boletin6_4/src/boletin6_4/Boletin6_4.java
+++ boletin6_4/src/boletin6_4/Boletin6_4.java
@@ -6,30 +6,25 @@ public class Boletin6_4 {
 
     public static void main(String[] args) {
         String nomePersoa1, nomePersoa2;
+        Peso obxPersoa= new Peso();
+        
         String nome1 = JOptionPane.showInputDialog("Introduce o nome da primeira persoa:");
         nomePersoa1 = (nome1);
+        obxPersoa.setNome1(nome1);
         String nome2 = JOptionPane.showInputDialog("Introduce o nome da segunda persoa:");
         nomePersoa2 = (nome2);
+        obxPersoa.setNome2(nome2);
 
-        short pesoPersoa1, pesoPersoa2;
+        float pesoPersoa1, pesoPersoa2;
         String peso1 = JOptionPane.showInputDialog("Introduce o peso da primeira persoa:");
-        pesoPersoa1 = Short.parseShort(peso1);
+        pesoPersoa1 = Float.parseFloat(peso1);
+        obxPersoa.setPeso1(pesoPersoa1);
         String peso2 = JOptionPane.showInputDialog("Introduce o peso da segunda persoa:");
-        pesoPersoa2 = Short.parseShort(peso2);
+        pesoPersoa2 = Float.parseFloat(peso2);
+        obxPersoa.setPeso2(pesoPersoa2);
 
         System.out.println("Persoa con maior peso:");
-
-        if (pesoPersoa1 > pesoPersoa2) {
-            System.out.println(nomePersoa1);
-            System.out.println(pesoPersoa1 + " kg");
-            System.out.println();
-            System.out.println("Diferenza de peso:\n" + (pesoPersoa1 - pesoPersoa2));
-        } else {
-            System.out.println(nomePersoa2);
-            System.out.println(pesoPersoa2 + " kg");
-            System.out.println();
-            System.out.println("Diferenza de peso:\n" + (pesoPersoa2 - pesoPersoa1));
+        
+        obxPersoa.compararPesos();
         }
     }
-
-}
