a893e667c1192614ca9962ef53aeb1e4213eba65
aperezcesar <aperezcesar@nautilus1.danielcastelao.org>
2018-11-05 12:38:40 +0100

    terminar

--- boletin6_7/src/boletin6_7/Boletin6_7.java
+++ boletin6_7/src/boletin6_7/Boletin6_7.java
@@ -1,23 +1,20 @@
-
 package boletin6_7;
 
 import java.util.Scanner;
 
-
 public class Boletin6_7 {
 
     public static void main(String[] args) {
-        Scanner sc= new Scanner (System.in);
+        Scanner sc = new Scanner(System.in);
         int op;
         Superficies figuras = new Superficies();
-        
+
         System.out.println("***********CÁLCULO DE SUPERFICIES***********\n"
                 + "1-> Cadrado\n2-> Triángulo\n3-> Círculo\nElixe unha opción:");
-        op= sc.nextInt();
+        op = sc.nextInt();
         figuras.setOpcion(op);
         figuras.calcularSuperficies();
-        
-        
+
     }
-    
+
 }
--- boletin6_7/src/boletin6_7/Superficies.java
+++ boletin6_7/src/boletin6_7/Superficies.java
@@ -1,48 +1,54 @@
-
 package boletin6_7;
 
 import java.util.Scanner;
 
 public class Superficies {
+
     private int opcion;
     private float superficie;
     private float base;
     private float altura;
     private float radio;
-    private final float PI=3.14f;
-    
-    Scanner sc= new Scanner(System.in);
-    public Superficies(){
+    private final float PI = 3.14f;
+
+    Scanner sc = new Scanner(System.in);
+
+    public Superficies() {
     }
-    
-    public void setSuperficie(float superf){
-        superficie= superf;
+
+    public void setSuperficie(float superf) {
+        superficie = superf;
     }
-    public void setOpcion(int opc){
-        opcion= opc;
+
+    public void setOpcion(int opc) {
+        opcion = opc;
     }
-    public void calcularSuperficies(){
-        switch(opcion){
-            case 1: System.out.println("Introduce a base:");
-            base= sc.nextFloat();
-            superficie= base*base;
-                System.out.println("Superficie do cadrado: "+superficie);
+
+    public void calcularSuperficies() {
+        switch (opcion) {
+            case 1:
+                System.out.println("Introduce a base:");
+                base = sc.nextFloat();
+                superficie = base * base;
+                System.out.println("Superficie do cadrado: " + superficie);
                 break;
-            case 2: System.out.println("Introduce a base:");
-            base= sc.nextFloat();
+            case 2:
+                System.out.println("Introduce a base:");
+                base = sc.nextFloat();
                 System.out.println("Introduce a altura:");
-            altura=sc.nextFloat();
-            superficie= base*(altura/2);
-                System.out.println("Superficie do triángulo: "+superficie);
+                altura = sc.nextFloat();
+                superficie = base * (altura / 2);
+                System.out.println("Superficie do triángulo: " + superficie);
                 break;
-            case 3: System.out.println("Introduce o radio:");
-                radio=sc.nextFloat();
-                superficie= PI*radio*radio;
-                System.out.println("Superficie do círculo: "+superficie);
+            case 3:
+                System.out.println("Introduce o radio:");
+                radio = sc.nextFloat();
+                superficie = PI * radio * radio;
+                System.out.println("Superficie do círculo: " + superficie);
                 break;
-            default: System.out.println("Opción non válida");
-            
-            
+            default:
+                System.out.println("Opción non válida");
+
         }
     }
 }
